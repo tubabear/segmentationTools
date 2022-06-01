0721bb9246c2ef27a1975060b441069f68b79005
TK <58165428+tubabear@users.noreply.github.com>
2022-06-01 13:26:03 +0800

    Add files via upload

---
+++ labelimg/libs/mask.py
@@ -0,0 +1,203 @@
+file_path = "/media/mmlab206/Data1/labelImg/data/0/IMG_7627.JPG"
+shapes = [{'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(168.82673267326714, 998.8861386138614), (433.82673267326714, 998.8861386138614), (433.82673267326714, 1532.8861386138615), (168.82673267326714, 1532.8861386138615)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(184.82673267326714, 1220.8861386138615), (416.82673267326714, 1220.8861386138615), (416.82673267326714, 1574.8861386138615), (184.82673267326714, 1574.8861386138615)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(343.82673267326714, 1079.8861386138615), (565.8267326732672, 1079.8861386138615), (565.8267326732672, 1656.8861386138615), (343.82673267326714, 1656.8861386138615)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(442.82673267326714, 1097.8861386138615), (799.8267326732672, 1097.8861386138615), (799.8267326732672, 1704.8861386138615), (442.82673267326714, 1704.8861386138615)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(653.8267326732672, 938.8861386138614), (1149.826732673267, 938.8861386138614), (1149.826732673267, 1724.8861386138615), (653.8267326732672, 1724.8861386138615)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(995.0, 821.0), (1552.0, 821.0), (1552.0, 1655.0), (995.0, 1655.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(1455.0, 774.0), (2102.0, 774.0), (2102.0, 1660.0), (1455.0, 1660.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(2001.0, 746.0), (2678.0, 746.0), (2678.0, 1644.0), (2001.0, 1644.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(2548.0, 776.0), (3162.0, 776.0), (3162.0, 1613.0), (2548.0, 1613.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(3037.0, 807.0), (3570.0, 807.0), (3570.0, 1664.0), (3037.0, 1664.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(3413.0, 919.0), (3854.0, 919.0), (3854.0, 1679.0), (3413.0, 1679.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(3733.0, 945.0), (4058.0, 945.0), (4058.0, 1667.0), (3733.0, 1667.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(3898.0, 853.0), (4248.0, 853.0), (4248.0, 1556.0), (3898.0, 1556.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(4042.0, 778.0), (4402.0, 778.0), (4402.0, 1276.0), (4042.0, 1276.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(3949.0, 1325.0), (4365.0, 1325.0), (4365.0, 1825.0), (3949.0, 1825.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(3898.0, 1466.0), (4143.0, 1466.0), (4143.0, 1900.0), (3898.0, 1900.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(3710.0, 1486.0), (4111.0, 1486.0), (4111.0, 2031.0), (3710.0, 2031.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(3456.0, 1442.0), (3922.0, 1442.0), (3922.0, 2175.0), (3456.0, 2175.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(3208.0, 1435.0), (3647.0, 1435.0), (3647.0, 2168.0), (3208.0, 2168.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(2823.0, 1409.0), (3358.0, 1409.0), (3358.0, 2314.0), (2823.0, 2314.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(2419.0, 1446.0), (2938.0, 1446.0), (2938.0, 2280.0), (2419.0, 2280.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(1998.0, 1423.0), (2553.0, 1423.0), (2553.0, 2225.0), (1998.0, 2225.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(1660.0, 1471.0), (2179.0, 1471.0), (2179.0, 2249.0), (1660.0, 2249.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(1243.0, 1456.0), (1838.0, 1456.0), (1838.0, 2331.0), (1243.0, 2331.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(951.0, 1500.0), (1371.0, 1500.0), (1371.0, 2307.0), (951.0, 2307.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(676.0, 1560.0), (1148.0, 1560.0), (1148.0, 2227.0), (676.0, 2227.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(489.0, 1576.0), (864.0, 1576.0), (864.0, 2214.0), (489.0, 2214.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(342.0, 1559.0), (601.0, 1559.0), (601.0, 2126.0), (342.0, 2126.0)], 'difficult': False}, {'label': 't', 'line_color': (182, 154, 142, 100), 'fill_color': (182, 154, 142, 100), 'points': [(213.0, 1497.0), (446.0, 1497.0), (446.0, 1979.0), (213.0, 1979.0)], 'difficult': False}]
+
+import sys
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog, QGridLayout, QLabel, QPushButton, QSlider
+from PyQt5.QtCore import Qt
+
+class createMask(QDialog):
+    def __init__(self, file_path, shapes, text="Create Mask.png", parent=None, list_item=None):
+        super(createMask, self).__init__(parent)
+
+        self.initUI()
+
+        # load image and shape
+        self.img = cv.imread(file_path)
+        self.shapes = shapes
+        points = [i for i in self.shapes[3]["points"]]
+        self.x1, self.y1, self.x2, self.y2 = int(points[0][0]), int(points[0][1]), int(points[1][0]), int(points[2][1])
+        self.max_val = 3000
+        self.min_val = 10
+        self.dil_it_val = 5
+        self.erod_it_val = 0
+        self.gaussian_val = 50
+        self.gau_it_val = 1
+        self.find_edge()
+
+    def initUI(self):
+
+        self.label = QLabel()
+        self.btnConfirm = QPushButton('Confirm', self)
+        
+        self.s1 = QSlider(Qt.Horizontal)
+        self.s1.setMaximum(5000)
+        self.s1.setMinimum(0)
+        self.s1.setValue(3000)
+        self.s1.setTickInterval(10)
+        self.s1.valueChanged.connect(self.max_val_change)
+
+        self.s2 = QSlider(Qt.Horizontal)
+        self.s2.setMaximum(5000)
+        self.s2.setMinimum(0)
+        self.s2.setValue(10)
+        self.s2.setTickInterval(5)
+        self.s2.valueChanged.connect(self.min_val_change)
+
+        self.dil_it = QSlider(Qt.Vertical)
+        self.dil_it.setMaximum(50)
+        self.dil_it.setMinimum(0)
+        self.dil_it.setValue(5)
+        self.dil_it.setTickInterval(5)
+        self.dil_it.valueChanged.connect(self.dil_it_change)
+
+        self.erod_it = QSlider(Qt.Vertical)
+        self.erod_it.setMaximum(50)
+        self.erod_it.setMinimum(0)
+        self.erod_it.setValue(0)
+        self.erod_it.setTickInterval(5)
+        self.erod_it.valueChanged.connect(self.erod_it_change)
+
+        self.gaussian = QSlider(Qt.Vertical)
+        self.gaussian.setMaximum(100)
+        self.gaussian.setMinimum(0)
+        self.gaussian.setValue(50)
+        self.gaussian.setTickInterval(5)
+        self.gaussian.valueChanged.connect(self.gaussian_val_change)
+        
+        self.gau_it = QSlider(Qt.Vertical)
+        self.gau_it.setMaximum(50)
+        self.gau_it.setMinimum(0)
+        self.gau_it.setValue(1)
+        self.gau_it.setTickInterval(5)
+        self.gau_it.valueChanged.connect(self.gau_it_val_change)
+
+        # self.btnSave = QPushButton('Save Image', self)
+        # self.btnSave.setEnabled(False)
+
+        layout = QGridLayout(self)
+        layout.addWidget(self.label, 0, 0, 4, 4)
+        layout.addWidget(self.btnConfirm, 4, 3, 1, 1)
+        layout.addWidget(self.s1, 4, 0, 1, 3)
+        layout.addWidget(self.s2, 5, 0, 1, 3)
+        layout.addWidget(self.dil_it, 0, 5, 4, 1)
+        layout.addWidget(self.erod_it, 0, 6, 4, 1)
+        layout.addWidget(self.gaussian, 0, 7, 2, 1)
+        layout.addWidget(self.gau_it, 2, 7, 2, 1)
+        # layout.addWidget(self.btnSave, 4, 2, 1, 1)
+
+        # self.btnConfirm.clicked.connect(self.openSlot)
+        # self.btnProcess.clicked.connect(self.processSlot)
+        # self.btnSave.clicked.connect(self.saveSlot)
+
+    def showImage(self, image):
+        height, width, channel = image.shape
+        bytesPerline = channel * width
+        self.qImg = QImage(image.data.tobytes(), width, height, bytesPerline, QImage.Format_RGB888).rgbSwapped()
+        self.label.setPixmap(QPixmap.fromImage(self.qImg))
+
+    def max_val_change(self):
+        self.max_val = int(self.s1.value())
+        print("max_edge: ", self.max_val, ";min_edge: ", self.min_val)
+        self.find_edge()
+
+    def min_val_change(self):
+        self.min_val = int(self.s2.value())
+        print("max_edge: ", self.max_val, ";min_edge: ", self.min_val)
+        self.find_edge()
+
+    def dil_it_change(self):
+        self.dil_it_val = int(self.dil_it.value())
+        print("dilate iteration:", self.dil_it_val,";erode iterations: ", self.erod_it_val)
+        self.find_edge()
+
+    def erod_it_change(self):
+        self.erod_it_val = int(self.erod_it.value())
+        print("dilate iteration:", self.dil_it_val,";erode iterations: ", self.erod_it_val)
+        self.find_edge()
+
+    def gaussian_val_change(self):
+        self.gaussian_val = int(self.gaussian.value())
+        print("gaussian value:", self.gaussian_val, ";gaussian iterations: ", self.gau_it_val)
+        self.find_edge()
+
+    def gau_it_val_change(self):
+        self.gau_it_val = int(self.gau_it.value())
+        print("gaussian value:", self.gaussian_val, ";gaussian iterations: ", self.gau_it_val)
+        self.find_edge()
+
+    def find_edge(self):
+
+        b = min(self.y2-self.y1, self.x2-self.x1)//20
+        # b = 0
+        raw_image = self.img[self.y1-b:self.y2+b, self.x1-b:self.x2+b].copy()
+        self.target = cv.GaussianBlur(raw_image, (5,5), self.gaussian_val).copy()
+        for i in range(self.gau_it_val):
+            self.target = cv.GaussianBlur(self.target, (5,5), self.gaussian_val)
+        cv.imshow("image", raw_image)
+
+        # grayscale
+        gray = cv.cvtColor(self.target, cv.COLOR_BGR2GRAY)
+
+        # mask
+        _, bw = cv.threshold(gray, 40, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+        mask = cv.add(self.target, np.zeros(raw_image.shape, raw_image.dtype), mask=bw)
+        # cv.imshow("mask", mask)
+
+        # canny edge
+        canny = cv.Canny(mask, self.min_val, self.max_val, L2gradient=True, apertureSize=7)
+        canny = cv.dilate(canny, (5,5), iterations=self.dil_it_val)
+        canny = cv.erode(canny, (5,5), iterations=self.erod_it_val)
+        # cv.imshow("canny", cv.bitwise_not(canny))
+
+        # connected component
+        mask_canny = cv.add(cv.bitwise_not(canny), np.zeros(canny.shape, canny.dtype), mask=bw)
+        num_lables, labels = cv.connectedComponents(mask_canny)
+        max_num = 1
+        count = (labels==1).sum()
+        for i in range(1, num_lables+1):
+            temp = (labels==i).sum()
+            if temp > count: 
+                max_num = i
+                count = temp
+        labels[labels!=max_num] = 0
+        # label_hue = np.uint8(179*labels/np.max(labels))
+        label_hue = np.uint8(labels*60)
+        blank_ch = 255*np.ones_like(label_hue)
+        labeled_img = cv.merge([label_hue, blank_ch, blank_ch])
+
+        labeled_img = cv.cvtColor(labeled_img, cv.COLOR_HSV2BGR)
+        labeled_img[label_hue==0] = 0
+        labeled_img = cv.add(raw_image, labeled_img)
+        # cv.imshow("labeled_img", labeled_img)
+
+        # 找最大contour
+        # ret, gray = cv.threshold(gray, self.min_val, self.max_val, 0)
+        contours, hierarchy = cv.findContours(canny, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        contours = sorted(contours, key=lambda x: cv.contourArea(x), reverse=True)
+        # contours, hierarchy = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        if len(contours) != 0:
+            # for i in contours:# 畫出所有contours
+                # img_contour = cv.drawContours(raw_image, i, -1, (255,0,0), 1)
+                # pass
+
+            # 畫最大contour
+            # img_contour = cv.drawContours(raw_image, contours[0], -1, (0,255,0), 3)
+            # cv.imshow("contours", img_contour)
+
+            # self.showImage(image=cv.cvtColor(canny, cv.COLOR_GRAY2RGB))
+            # self.showImage(image=img_contour)
+            self.showImage(image=labeled_img)
+
+        else:
+            self.showImage(image=self.target)
+
+
+if __name__ == '__main__':
+    a = QApplication(sys.argv)
+    dialog = createMask(file_path=file_path, shapes=shapes)
+    dialog.show()
+    sys.exit(a.exec_())
